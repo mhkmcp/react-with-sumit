c4ef9fdc8e08684b034b146744f7d1aa623f4b39
mhkmcp <mhkmcp@yahoo.com>
2021-05-13 02:55:20 +0600

    Lesson-07: State & Lifecycle in class Component

--- src/ClockClass.js
+++ src/ClockClass.js
@@ -1,10 +1,30 @@
 import { React, Component } from 'react'
 
 class ClockElement extends Component {
-    state = {  }
+    // when state is dependent on props 
+    // constructor(props) {
+    //     super(props);
+    //     this.state = { date: props.date }
+    // }
+    
+    // when state is independent of props
+    state = { date: new Date() }
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => { this.tick() }, 1000); 
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+    
+    tick() {
+        this.setState({date: new Date()})
+    }
+    
     render() { 
         return ( 
-            <div>Here's the { this.props.children } Time is {new Date().toLocaleTimeString(this.props.locale)}</div>
+            <div>Here's the { this.props.children } Time is {this.state.date.toLocaleTimeString(this.props.locale)}</div>
          );
     }
 }
